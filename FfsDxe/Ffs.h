559aae13002ff44ee96ac9d808d581ee71e0730f
Colin Drake <colin.f.drake@gmail.com>
2011-05-23 14:18:01 -0400

    Include header

---
+++ FfsDxe/Ffs.h
@@ -0,0 +1,12 @@
+/*
+
+  FileSystemPkg - Ffs.h
+
+  Copyright (c) 2011, Colin Drake <colin.f.drake@gmail.com>
+
+  This program and the accompanying materials are licensed and made available
+  under the terms and conditions of the Software License Agreement which
+  accompanies this distribution.
+
+*/
+
