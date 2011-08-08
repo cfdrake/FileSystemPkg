735506475e9b4410fb56d5bd3977e0da5fb5b895
Jordan Justen <jljusten@gmail.com>
2011-08-07 21:05:30 -0700

    FileSystemPkg/FfsDxe: Fix read-only open check
    Signed-off-by: jljusten

--- FfsDxe/Ffs.c
+++ FfsDxe/Ffs.c
@@ -493,8 +493,7 @@ FfsOpen (
   // Check for a valid OpenMode parameter. Since this is a read-only filesystem
   // it must not be EFI_FILE_MODE_WRITE or EFI_FILE_MODE_CREATE. Additionally,
   // ensure that the file name to be accessed isn't empty.
-  if (OpenMode & EFI_FILE_MODE_READ ||
-      OpenMode & EFI_FILE_MODE_CREATE) {
+  if ((OpenMode & (EFI_FILE_MODE_READ | EFI_FILE_MODE_CREATE)) != EFI_FILE_MODE_READ) {
     DEBUG ((EFI_D_INFO, "FfsOpen: OpenMode must be Read\n"));
     return EFI_WRITE_PROTECTED;
   } else if (FileName == NULL || StrCmp (FileName, L"") == 0) {
